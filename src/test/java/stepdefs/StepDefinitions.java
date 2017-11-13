a5c33aecd77a04bc6c3db9feeb802c4388ff07e3
karwinski <carlosfrancisco.esponda.contractor@datiobd.com>
2017-11-13 00:38:33 -0600

    Adding funcionality to validate JSON body response

--- src/test/java/stepdefs/StepDefinitions.java
+++ src/test/java/stepdefs/StepDefinitions.java
@@ -1,28 +1,28 @@
 package stepdefs;
 
-import cucumber.api.PendingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.testng.Assert;
-import org.testng.AssertTest;
 
-import java.net.URL;
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.*;
-import javax.net.ssl.HttpsURLConnection;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.HttpURLConnection;
-import java.net.*;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.net.URLEncoder;
-
-
+import java.net.URLEncoder.*;
 
 public class StepDefinitions {
 
     private URL url;
     private HttpURLConnection con;
     private int responseCode;
-
+    private StringBuffer response;
 
 
     @Given("A connection to \"([^\"]*)\"$")
@@ -33,14 +33,14 @@ public class StepDefinitions {
 
         try {
             con = (HttpURLConnection) url.openConnection(); //-->> conection
-        } catch (IOException e) {
+        } catch (IOException e){
             e.printStackTrace();
         }
 
-        System.out.println("conection succesfull");
+        System.out.println("conecction succesfull");
     }
 
-    @When("User make a get request")
+    @When("User makes a get request")
     public void getRequest() throws IOException {
 
         con.setRequestMethod("GET");
@@ -57,4 +57,28 @@ public class StepDefinitions {
         Assert.assertEquals(responseCode,number,"Responses code are not matching");
 
     }
+
+
+    //@And("^The content is shown on the results:$")
+    @And("^the JSON response should have origin with value \"(\\d+)\\.(\\d+)\\.(\\d+)\\.(\\d+)\"$")
+    public void body_content(int arg1, int arg2, int arg3, int arg4) throws IOException {
+        BufferedReader  in = new BufferedReader(
+                new InputStreamReader(con.getInputStream()));
+        String inputLine;
+        response = new StringBuffer();
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
+        }
+        //ObjectMapper mapper = new ObjectMapper();
+
+        //String jsonStr = mapper.writeValueAsString(response);
+        //result = mapper.readValue(jsonStr,StringBuffer.class);
+
+        System.out.println(response);
+        Assert.assertEquals(arg1+"."+arg2+"."+arg3+"."+arg4, response.substring(14,29),"Body content are not matching");
+
+    }
+
+
+
 }
--- src/test/resources/features/Test.feature
+++ src/test/resources/features/Test.feature
@@ -1,11 +1,25 @@
 Feature: Test rest api
-  As a QA
+  As a QA of Datio company
   I want to validate endpoints [get and post requests]
   So that will reduce the time that developers invest
   in debugging errors and finally it will reduce costs.
-
-  Scenario: Get
-    Given A connection to "https://httpbin.org/ip"
-    When  User make a get request
+  Scenario: Succesfull status response and body content
+    Given A connection to "http://httpbin.org/ip"
+    When  User makes a get request
     Then  The status response should be 200
-    
+    And the JSON response should have origin with value "187.190.164.238"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
